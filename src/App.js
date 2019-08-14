c75d6ad2faed91097ddad79239ced7ddefe97bd1
tangqi <15827394900@163.com>
2019-08-14 09:56:37 +0800

    完成了一些配置

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router} from "react-router-dom";
+
+export default class  extends Component {
+  render() {
+    return <Router>
+      App
+    </Router>;
+  }
+}
