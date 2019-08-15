bccad9bab02a3487f229fff0f5de7e49a82469b6
tangqi <15827394900@163.com>
2019-08-15 17:26:14 +0800

    完成了登录校验

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -6,8 +6,31 @@ import logo from "./logo.png";
 import "./index.less";
 //变量
 const Item = Form.Item;
-export default class Login extends Component {
+
+ class Login extends Component {
+   //这是自定义校正方法 validator
+   //检验的input组件
+   //校验的值
+   //callback()没有值是就代表成功 有值代表校验失败 
+   validator =(rule, value, callback) => {
+     const passwordReg = /^\w+$/;
+     const name = rule.field === "username"?"用户名":"密码";
+     if (!value){
+       callback(`${name}输入的值不能为空`);
+     } else if (value.length<4) {
+       callback(`${name}输入的值不能小于4位`);
+     } else if (value.length>10) {
+       callback(`${name}输入的值不能大于10位`);
+     }
+     else if (!passwordReg.test(value)) {
+       callback(`${name}只能包含英文，数字，下划线`);
+     }
+     callback();
+   };
   render() {
+    //这是从form中提取的一个属性 作用是表单验证
+//用法  {getFieldDecorator (标识符获取值的时候用的到，{rule:[{}]}) (需要校验的组件)}
+    const { getFieldDecorator } = this.props.form;
     return <div className="login">
       <header className="login-header">
         <img src={logo} alt="logo"/>
@@ -18,17 +41,78 @@ export default class Login extends Component {
         <h2>用户登录</h2>
         <Form onSubmit={this.login}>
           <Item>
-            <Input prefix={ <Icon type="user" /> } placeholder="用户名"/>
+      {/*      {
+              getFieldDecorator
+              (
+                'username',{
+                  rules: [
+                    {
+                      required: true,
+                      message: '用户名不能为空'
+                    },
+                    {
+                      min:4 ,
+                      message: '用户名最少输入4位'
+                    },
+                    {
+                      max:10 ,
+                      message: '用户名最多输入10位'
+                    },
+                    {
+                      pattern:/^\w+$/,
+                      message: '用户名只能包含英文，数字，下划线'
+                    }
+                  ]
+                }
+              )
+              (
+                <Input prefix={ <Icon type="user" /> } placeholder="用户名"/>
+              )
+            }
+*/}
+
+            {
+              getFieldDecorator
+              (
+                'username',{
+                  rules: [
+                    { validator:this.validator }
+                  ]
+                }
+              )
+              (
+                <Input prefix={ <Icon type="user" /> } placeholder="用户名"/>
+              )
+            }
+
           </Item>
           <Item>
-            <Input prefix={ <Icon type="lock" /> } placeholder="密码"/>
+            {
+              getFieldDecorator
+              (
+                'password',{
+                  rules: [
+                    { validator:this.validator }
+                  ]
+                }
+              )
+              (
+                <Input prefix={ <Icon type="lock" /> } placeholder="密码"/>
+              )
+            }
           </Item>
           <Item>
             <Button type="primary" htmlType="submit" >登录</Button>
           </Item>
         </Form>
-    </div>
+      </div>
       </section>
     </div>
   }
 }
+//From.create 给Login组件添加 from的属性到props上用来做表单验证
+//他是一个高阶组件，传入一个组件返回一个新组件
+//新组件具备表单校验的功能
+//经过 Form.create 包装的组件将会自带 this.props.form 属性，this.props.form 提供的 API
+const newLogin = Form.create () (Login);
+export default newLogin
